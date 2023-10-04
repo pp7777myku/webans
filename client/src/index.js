2cb058b2b5236299d6f6c29ec4cffacd0a482a53
LI Ziming <62941440+pp7777myku@users.noreply.github.com>
2023-10-04 18:25:32 +0300

    Delete client directory

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter, Router, Routes} from 'react-router-dom'
-import App from './App';
-
-
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
-
